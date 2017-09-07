4354aa2423027d98201cb215773e50973e611d3a
kenta <moka12almukha30@gmail.com>
2017-09-07 14:32:06 +0900

    update .ino
    プロポからの入力ピン設定

---
+++ DecoROBOCON/DecoROBOCON.ino
@@ -0,0 +1,21 @@
+//プロポからの入力ピン
+const int R1 = 12;
+const int R2 = 14;
+const int R3 = 27;
+const int R4 = 26;
+//プロポからの入力用変数
+
+
+void setup()
+{
+  pinMode(R1,INPUT);
+  pinMode(R2,INPUT);
+  pinMode(R3,INPUT);
+  pinMode(R4,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
