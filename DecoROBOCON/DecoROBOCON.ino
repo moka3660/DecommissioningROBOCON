6f7084d7cc5757e77dd1f5cdf0204ae54f8dfa51
kenta <moka12almukha30@gmail.com>
2017-09-14 11:57:34 +0900

    fix DecoROBOCON.ino
    1)入力ピンの変数名をRからPin_xxxに変更
    2)入力ピン,出力ピンの修正

--- DecoROBOCON/DecoROBOCON.ino
+++ DecoROBOCON/DecoROBOCON.ino
@@ -4,26 +4,26 @@ Servo Elevator;
 Servo Throttle;
 Servo Ladder;
 //プロポからの入力ピン
-const int R1 = 2;
-const int R2 = 15;
-const int R3 = 14;
-const int R4 = 12;
+const int Pin_ail = 12;  //エルロン
+const int Pin_ele = 14;  //エレベーター
+const int Pin_thr = 15;  //スロットル
+const int Pin_lad = 2;   //ラダー
 //プロポからの入力用変数
 int rcv[] = {0, 0, 0, 0};
 //フラコンへの出力ピン
-const int Pin_ail = 9;
-const int Pin_ele = 13;
-const int Pin_thr = 11;
-const int Pin_lad = 10;
+const int Pout_ail = 13;
+const int Pout_ele = 9;
+const int Pout_thr = 10;
+const int Pout_lad = 11;
 //フラコンへの出力用変数
 int snd[] = {1520, 1520, 1109, 1520};
 
 void setup()
 {
-  pinMode(R1,INPUT);
-  pinMode(R2,INPUT);
-  pinMode(R3,INPUT);
-  pinMode(R4,INPUT);
+  pinMode(Pin_ail,INPUT);
+  pinMode(Pin_ele,INPUT);
+  pinMode(Pin_thr,INPUT);
+  pinMode(Pin_lad,INPUT);
 
   //servo.attach(pin, min, max)
   Aileron.attach(Pin_ail, 1109, 1930);
@@ -37,10 +37,10 @@ void setup()
 
 void loop()
 {
-  rcv[0] = pulseIn(R1, HIGH);
-  rcv[1] = pulseIn(R2, HIGH);
-  rcv[2] = pulseIn(R3, HIGH);
-  rcv[3] = pulseIn(R4, HIGH);
+  rcv[0] = pulseIn(Pin_ail, HIGH);
+  rcv[1] = pulseIn(Pin_ele, HIGH);
+  rcv[2] = pulseIn(Pin_thr, HIGH);
+  rcv[3] = pulseIn(Pin_lad, HIGH);
 /*
   Serial.print("___rcv[0]:");
   Serial.print(rcv[0]);
