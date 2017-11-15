4aae80e9f107c71d4893ac4da10bfac7e9284756
kenta <moka12almukha30@gmail.com>
2017-11-15 15:32:25 +0900

    fix & change
    1)サーボ指定ピンをinのにしてたため不具合があった->修正済
    2)わかりやすいようにPinとPoutをIとOに変更
    3)debugの変更については無視する方向で

--- DecoRBCN_debug/DecoRBCN_debug.ino
+++ DecoRBCN_debug/DecoRBCN_debug.ino
@@ -5,6 +5,7 @@ Servo Aileron;
 Servo Elevator;
 Servo Throttle;
 Servo Ladder;
+
 //プロポからの入力ピン
 const int Pin_ail = 2;  //エルロン
 const int Pin_ele = 3;  //エレベーター
@@ -12,13 +13,16 @@ const int Pin_thr = 4;  //スロットル
 const int Pin_lad = 5;   //ラダー
 //プロポからの入力用変数
 int rcv[] = {0, 0, 0, 0};
+
 //フラコンへの出力ピン
 const int Pout_ail = 6;
 const int Pout_ele = 7;
 const int Pout_thr = 8;
 const int Pout_lad = 9;
+
 //フラコンへの出力用変数
 int snd[] = {1520, 1520, 1109, 1520};
+
 //I2Cレーザ測距
 VL53L0X ahead;  //前方
 
@@ -30,10 +34,10 @@ void setup()
   pinMode(Pin_lad,INPUT);
 
   //servo.attach(pin, min, max)
-  Aileron.attach(Pin_ail, 1109, 1930);
-  Elevator.attach(Pin_ele, 1109, 1930);
-  Throttle.attach(Pin_thr, 1109, 1930);
-  Ladder.attach(Pin_lad, 1109, 1930);
+  Aileron.attach(Pout_ail, 1109, 1930);
+  Elevator.attach(Pout_ele, 1109, 1930);
+  Throttle.attach(Pout_thr, 1109, 1930);
+  Ladder.attach(Pout_lad, 1109, 1930);
 
   Wire.begin();
 
@@ -76,5 +80,5 @@ void loop()
   Throttle.writeMicroseconds(snd[2]);
   Ladder.writeMicroseconds(snd[3]);
 
-  //delay(500);
+  delay(50);
 }
--- DecoROBOCON/DecoROBOCON.ino
+++ DecoROBOCON/DecoROBOCON.ino
@@ -6,17 +6,17 @@ Servo Elevator;
 Servo Throttle;
 Servo Ladder;
 //プロポからの入力ピン
-const int Pin_ail = 2;  //エルロン
-const int Pin_ele = 3;  //エレベーター
-const int Pin_thr = 4;  //スロットル
-const int Pin_lad = 5;   //ラダー
+const int I_ail = 2;  //エルロン
+const int I_ele = 3;  //エレベーター
+const int I_thr = 4;  //スロットル
+const int I_lad = 5;   //ラダー
 //プロポからの入力用変数
 int rcv[] = {0, 0, 0, 0};
 //フラコンへの出力ピン
-const int Pout_ail = 6;
-const int Pout_ele = 7;
-const int Pout_thr = 8;
-const int Pout_lad = 9;
+const int O_ail = 6;
+const int O_ele = 7;
+const int O_thr = 8;
+const int O_lad = 9;
 //フラコンへの出力用変数
 int snd[] = {1520, 1520, 1109, 1520};
 //I2Cレーザ測距
@@ -24,16 +24,16 @@ VL53L0X ahead;  //前方
 
 void setup()
 {
-  pinMode(Pin_ail,INPUT);
-  pinMode(Pin_ele,INPUT);
-  pinMode(Pin_thr,INPUT);
-  pinMode(Pin_lad,INPUT);
+  pinMode(I_ail,INPUT);
+  pinMode(I_ele,INPUT);
+  pinMode(I_thr,INPUT);
+  pinMode(I_lad,INPUT);
 
   //servo.attach(pin, min, max)
-  Aileron.attach(Pin_ail, 1109, 1930);
-  Elevator.attach(Pin_ele, 1109, 1930);
-  Throttle.attach(Pin_thr, 1109, 1930);
-  Ladder.attach(Pin_lad, 1109, 1930);
+  Aileron.attach(O_ail, 1109, 1930);
+  Elevator.attach(O_ele, 1109, 1930);
+  Throttle.attach(O_thr, 1109, 1930);
+  Ladder.attach(O_lad, 1109, 1930);
 
   Wire.begin();
 
@@ -47,10 +47,10 @@ void setup()
 
 void loop()
 {
-  rcv[0] = pulseIn(Pin_ail, HIGH);
-  rcv[1] = pulseIn(Pin_ele, HIGH);
-  rcv[2] = pulseIn(Pin_thr, HIGH);
-  rcv[3] = pulseIn(Pin_lad, HIGH);
+  rcv[0] = pulseIn(I_ail, HIGH);
+  rcv[1] = pulseIn(I_ele, HIGH);
+  rcv[2] = pulseIn(I_thr, HIGH);
+  rcv[3] = pulseIn(I_lad, HIGH);
 
   Serial.print("___rcv[0]:");
   Serial.print(rcv[0]);
