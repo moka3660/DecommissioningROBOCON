f473a8952bfba8bb351dacd736b0625e73e481ee
moka3660 <moka12almukha30@gmail.com>
2017-12-10 20:39:52 +0900

    update
    XSHUTピンでアドレス設定できた

--- Laser_set/Laser_set.ino
+++ Laser_set/Laser_set.ino
@@ -3,6 +3,7 @@
 
 //I2Cレーザ測距
 VL53L0X laser;  //
+VL53L0X sencer;
 
 void setup()
 {
@@ -10,11 +11,15 @@ void setup()
 
   pinMode(6,OUTPUT);
 
+  digitalWrite(6,LOW);
   laser.init();
   laser.setTimeout(500);
-//  laser.setAddress(21);
-//  laser.writeReg(0x52, 0x21);
+  laser.setAddress(21);
   laser.startContinuous();
+  digitalWrite(6,HIGH);
+  sencer.init();
+  sencer.setTimeout(500);
+  sencer.startContinuous();
 
   Serial.begin(9600); //シリアルモニタで確認
   Serial.println("started");
@@ -22,21 +27,21 @@ void setup()
 
 void loop()
 {
-  digitalWrite(6,HIGH);
-  Serial.print("HIGH---Laser[mm]---");
+//  digitalWrite(6,HIGH);
+  Serial.print("laser---Laser[mm]---");
   Serial.print(laser.readRangeContinuousMillimeters());
   Serial.print("---Address----");
   Serial.print(laser.getAddress());
 
-  Serial.println();
+//  Serial.println();
 
-  delay(500);
+//  delay(500);
 
-  digitalWrite(6,LOW);
-  Serial.print("LOW---Laser[mm]---");
-  Serial.print(laser.readRangeContinuousMillimeters());
+//  digitalWrite(6,LOW);
+  Serial.print("sencer---Laser[mm]---");
+  Serial.print(sencer.readRangeContinuousMillimeters());
   Serial.print("---Address----");
-  Serial.print(laser.getAddress());
+  Serial.print(sencer.getAddress());
 
   Serial.println();
 
