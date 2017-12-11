d603afc069e3c41870a4e4e1166c39a9fd51f584
moka3660 <moka12almukha30@gmail.com>
2017-12-11 11:04:41 +0900

    update DecoROBOCON
    1)LEDのピン設定追加
    2)レーザ用変数distance追加
    3)loop内のをdistance使用に変更

--- DecoROBOCON/DecoROBOCON.ino
+++ DecoROBOCON/DecoROBOCON.ino
@@ -1,16 +1,19 @@
 #include<Wire.h>
 #include<VL53L0X.h>
 
+const int LED = A0; //レーザ検知したら光らすLED
+int distance = 0;   //レーザ距離用変数
 //I2Cレーザ測距
-VL53L0X downward;  //下方
+VL53L0X laser;  //下方
 
 void setup()
 {
+  pinMode(LED,OUTPUT);
   Wire.begin();
 
-  downward.init();
-  downward.setTimeout(500);
-  downward.startContinuous();
+  laser.init();
+  laser.setTimeout(500);
+  laser.startContinuous();
 
   Serial.begin(9600); //シリアルモニタで確認
   Serial.println("started");
@@ -18,9 +21,10 @@ void setup()
 
 void loop()
 {
+  distance = laser.readRangeContinuousMillimeters();
 
-  Serial.print("---Laser---___ahead:");
-  Serial.print(downward.readRangeContinuousMillimeters());
+  Serial.print("---Laser---:");
+  Serial.print(distance);
 
   Serial.println();
 
